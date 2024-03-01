d6c73d62014cbbf5c41328e777a3f3a1b5b7eb00
Baishali-Saha <161792002+Baishali-Saha@users.noreply.github.com>
2024-03-01 08:40:33 +0530

    Create config.py

---
+++ handlers/config.py
@@ -0,0 +1,20 @@
+configurations = {
+    "1":{
+        "que":"what is the capital of India?" ,
+        "options": " A. Jaipur \n B. New Delhi \n C. Kokrajhar \n D. New York \n",
+        "answer" : "B"    
+    },
+    "2":{
+        "que":"what is the capital of Assam?",
+        "options": " A. Jaipur \n B. New Delhi \n C. Kokrajhar \n D. Dispur \n",
+        "answer" : "D"    
+    },
+    "3":{
+        "que":"what is the capital of USA?",
+        "options": " A. Jaipur \n B. New Delhi \n C. Washington DC \n D. Dispur \n",
+        "answer" : "C"    
+    }, 
+}
+
+score = 0
+
